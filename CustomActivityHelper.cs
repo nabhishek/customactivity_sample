68a726d893ae86dc5beacc193df31edfecaec0de
tongwu <tongwu@microsoft.com>
2018-05-08 11:42:47 +0800

    Sample for custom activity

---
+++ CustomActivityHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using CleanupTask.Properties;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CleanupTask
+{
+    /// <summary>
+    /// Used for parse 
+    /// </summary>
+    internal static class CustomActivityHelper
+    {
+        internal static async Task<JObject> ParseActivityFromInputFileAsync(bool resolveAkvSecret = false)
+        {
+            if (!File.Exists("activity.json"))
+            {
+                throw new FileNotFoundException("activity.json");
+            }
+
+            Console.WriteLine("Start to parse Activity from input file.");
+            // Read Activity Object from input files
+            JObject activity = (JObject)JsonConvert.DeserializeObject(File.ReadAllText("activity.json"));
+
+            if (resolveAkvSecret)
+            {
+                Console.WriteLine("Start to resolve akv secret in Activity.");
+                activity = await ResolveAkvSecretAsync(activity);
+                Console.WriteLine("Resolve akv secret in Activity Complete.");
+            }
+
+            return activity;
+        }
+
+        /***
+         * Resolve any property in JObject with below AKV structure
+         * 
+         * {
+         *       "type": "AzureKeyVaultSecret",
+         *       "secretName": "<your secret name>",
+         *       "store": {
+         *           "referenceName": "<your akv linked service name>",
+         *           "type": "LinkedServiceReference"
+         *      }
+         * }
+         * 
+         */
+        internal static async Task<JObject> ResolveAkvSecretAsync(JObject obj)
+        {
+            if (!File.Exists("linkedServices.json"))
+            {
+                throw new FileNotFoundException("linkedServices.json");
+            }
+
+            // Select all linked service with type == AzureKeyVault
+            IEnumerable<JToken> akvLinkedServices = 
+                ((JArray)JsonConvert.DeserializeObject(File.ReadAllText("linkedServices.json"))).SelectTokens("$[?(@.properties.type == 'AzureKeyVault')]");
+
+            if (akvLinkedServices == null || akvLinkedServices.Count() == 0)
+            {
+                return null;
+            }
+
+            // Collect all base urls from akv linked services
+            Dictionary<string, string> akvUrls = new Dictionary<string, string>();
+            foreach (JToken t in akvLinkedServices)
+            {
+                akvUrls[(string)t["name"]] = (string)t.SelectToken("$.properties.typeProperties.baseUrl");
+            }
+
+            // Get all akv secrets in JObject
+            // JPath is not well supported in all Newton.Json version. 
+            JToken[] akvSecretToken = obj.SelectTokens("$..*").Where(_ =>
+            {
+                if (!_.HasValues || _ is JArray)
+                {
+                    return false;
+                }
+                
+                return "AzureKeyVaultSecret".Equals(_.Value<string>("type"));
+            }).ToArray();
+            Console.WriteLine($"{akvSecretToken.Count()} secrets found.");
+
+            // Use Cert to access AKV, can be replace to use key or token.
+            X509Certificate2 cert = FindCertificateByThumbprint(Settings.Default.Thumbprint);
+            foreach (var token in akvSecretToken)
+            {
+                string secretName = (string)token["secretName"];
+                string akvLsName = token.GetProperty<string>("$.store.referenceName");
+                string baseUrl = akvUrls[akvLsName];
+
+                string value = await RetrieveSecretAsync(baseUrl, 
+                                                         Settings.Default.ClientId,
+                                                         cert,
+                                                         secretName);
+                token.Replace(JToken.FromObject(value));
+            }
+
+            return obj;
+        }
+
+        internal static T GetProperty<T>(this JToken obj, string jPath, bool throwExceptionIfNotFound = true)
+        {
+            var token = obj.SelectToken(jPath);
+            if (token == null && throwExceptionIfNotFound)
+            {
+                throw new FormatException($"{jPath} not found.");
+            }
+
+            return token != null ? token.Value<T>() : default(T);
+        }
+
+        // Use Cert to access AKV
+        private static async Task<string> RetrieveSecretAsync(string akvBaseUrl, string clientId, X509Certificate2 cert, string secretName)
+        {
+            var assertionCert = new ClientAssertionCertificate(clientId, cert);
+            var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(async (authority, resource, scope) =>
+            {
+                var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+
+                var result = await context.AcquireTokenAsync(resource, assertionCert);
+                return result.AccessToken;
+            }));
+
+            SecretBundle secretBundle = await kvClient.GetSecretAsync(akvBaseUrl, secretName);
+            return secretBundle != null ? secretBundle.Value : null;
+        }
+
+
+        private static X509Certificate2 FindCertificateByThumbprint(string thumbprint)
+        {
+            X509Store store = null;
+            try
+            {
+                store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+
+                return certCollection == null || certCollection.Count == 0
+                        ? null
+                        : certCollection[0];
+            }
+            finally
+            {
+                if (store != null)
+                {
+                    store.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CleanupTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start to execute custom activity.");
+            try
+            {
+                ExcuteCustomActivityAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                ex = ex.InnerException ?? ex;
+
+                Console.Error.WriteLine($"Custom activity execution failed: {ex.Message}");
+                Console.Error.WriteLine($"StackTrace {ex.StackTrace}");
+                throw;
+            }
+
+            Console.WriteLine("Execute custom activity complete.");
+        }
+
+        static async Task ExcuteCustomActivityAsync()
+        {
+            JObject activity = await CustomActivityHelper.ParseActivityFromInputFileAsync(true);
+
+            string connectionString = activity.GetProperty<string>(@"$..extendedProperties.connectionString");
+            string folderName = activity.GetProperty<string>(@"$..extendedProperties.folderPath");
+            string containerName = activity.GetProperty<string>(@"$..extendedProperties.container");
+
+            // Create blob client
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Delete all files in folder
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            CloudBlobDirectory folder = container.GetDirectoryReference(folderName);
+            bool result = false;
+            List<string> deleteFiles = new List<string>();
+            foreach (IListBlobItem blob in folder.ListBlobs(true))
+            {
+                if (blob.GetType() == typeof(CloudBlob) || blob.GetType().BaseType == typeof(CloudBlob))
+                {
+                    string deletedBlobUri = blob.StorageUri.ToString();
+                    result |= ((CloudBlob)blob).DeleteIfExists();
+                    deleteFiles.Add(deletedBlobUri);
+                }
+            }
+
+            Console.WriteLine($"{containerName}:{folderName} delete result: {result}");
+
+            // Put result to output of custom activity
+            if (deleteFiles != null)
+            {
+                File.WriteAllText("outputs.json", JsonConvert.SerializeObject(new { deletedFile = deleteFiles }));
+            }
+        }
+    }
+}
